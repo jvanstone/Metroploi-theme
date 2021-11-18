e4259ab5b8b301040b248b0e36b8f0c1042e1319
Jason Vanstone <jason@vanstoneonline.com>
2021-11-18 13:34:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blank Template Astra Child Theme
+
+This child theme is to be used in conjunction with Astra
+
+
+- Astra CSS is dequeued to help with load timess
+- Editor Syle is activated.
+- Customize the theme color with Theme.json 
+- SCSS file created to help with customizing the theme
---
+++ functions.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * American Lanterns Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package AmericanLanterns
+ * @since 1.0.0
+ */
+
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles, dequeue ASTRA stylesheet.
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style(
+		'zpv-theme-css',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( 'astra-theme-css' ),
+		CHILD_THEME_VERSION,
+		'all'
+	);
+
+	wp_enqueue_style( 'dashicons' );
+
+	wp_dequeue_style( 'astra-theme-css' );
+}
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' );
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
+add_theme_support( 'editor-style' );
+
+/**
+ * Registers an editor stylesheet in a sub-directory.
+ */
+function add_editor_styles_sub_dir() {
+	add_editor_style(
+		trailingslashit( get_template_directory_uri() ) . 'editor-style.css'
+	);
+}
+add_action( 'after_setup_theme', 'add_editor_styles_sub_dir' );
